d0e87bf6be489b74bcac460bb4484af6baedd562
thanuz thummala <thanuz.thummala@oracle.com>
2025-01-24 16:33:38 -0500

    removing

--- index.js
+++
@@ -1,7 +0,0 @@
-import open from "open";
-
-const url = "https://devops.oci.oraclecorp.com/shepherd/projects/maui-templates/flocks/maui-preact-plugin/releases";
-
-open(url).catch((err) => {
-    console.error("Failed to open URL:", err);
-});
