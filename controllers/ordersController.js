347a0490c6ffbf1c617ee2da7f6f53f6918f107b
Thanuz Thummala <tummala.royels@gmail.com>
2025-02-04 12:38:26 -0500

    adding total sold price in getallorders endpoint

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -3,7 +3,18 @@ const { getAllOrdersDetails } = require("../services/ordersService");
 async function fetchAllOrders(req, res) {
   try {
     const products = await getAllOrdersDetails();
-    res.status(200).json(products); // Send data as JSON response
+    let totalSoldCost = 0;
+    if (products.length > 0) {
+      totalSoldCost = products.reduce((acc, cur) => {
+        const { sold_price, quantities_sold } = cur;
+        acc += parseFloat(sold_price) * quantities_sold;
+        return acc;
+      }, 0);
+    }
+
+    console.log(totalSoldCost);
+    const result = { totalSoldCost, soldProductDetails: products };
+    res.status(200).json(result); // Send data as JSON response
   } catch (error) {
     console.error("Error in fetchingAllOrders:", error);
     res
